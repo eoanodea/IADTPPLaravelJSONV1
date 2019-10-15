4773eee72ae33b0ae0d950f9fe28c6b7d0d764e2
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-15 19:12:31 +0100

    Began implementing Laravel JSON response backend following this tutorial - https://www.toptal.com/laravel/restful-laravel-api-tutorial

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Article;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Truncate our existing records to start from scratch
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        //Create a few articles in our DB
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
