c1bd60365d4f3e5ad9ca8bf861fbeebdedf0f0ae
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-17 16:32:07 +0100

    Further implementation of RegisterController

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support;
 
 class User extends Authenticatable
 {
@@ -37,8 +38,31 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    private function generateRandomString() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+        // 32 bits for "time_low"
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+        // 16 bits for "time_mid"
+        mt_rand(0, 0xffff),
+
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand(0, 0x0fff) | 0x4000,
+
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand(0, 0x3fff) | 0x8000,
+
+        // 48 bits for "node"
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+
     public function generateToken() {
-        $this->api_token = str_random(60);
+        $this->api_token = generateRandomString();
         $this->save();
 
         return $this->api_token;
